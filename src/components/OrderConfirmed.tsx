24ae416ccb404f3a8c77cfb07d91e3bd78446cea
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-07-20 17:32:03 +0100

    testing

--- src/components/OrderConfirmed.tsx
+++ src/components/OrderConfirmed.tsx
@@ -13,16 +13,16 @@ const OrderConfirmed: React.FC<props> = ({item, cart}) => {
   const flexDiv: string = 'flex'
   
   return (
-    <div className='border-b-[1.5px] border-Rose300/50 py-3 ssm:py-3'>
+    <div className='border-b-[1.5px] border-Rose300/50 py-3'>
       <div className={`${flexDiv} items-center`}>
         <img 
           src={item.image.thumbnail} 
           alt={`${item.name}-thumbnail`}
-          className='size-12 object-cover object-center rounded-md mr-4'
+          className='size-[45px] object-cover object-center rounded-md mr-3'
         />
         <div className={`${flexDiv} flex-col items-start`}>
           <p className="font-semibold block w-[150px] whitespace-nowrap
-          overflow-hidden text-ellipsis text-[15px] pb-[3px]"
+          overflow-hidden text-ellipsis text-[15px] pb-[2px]"
           >
             {item.name}</p>
             <span className={`${flexDiv} gap-4 font-bold text-Red`}>x{item.quantity || 1} 
